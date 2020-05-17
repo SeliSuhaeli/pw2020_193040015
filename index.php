c92ba85290594f75c75db65428021a2230228624
SeliSeptian <SeliSeptian@users.noreply.github.com>
2020-05-17 23:03:42 +0700

    TUGAS BESAR

---
+++ index.php
@@ -0,0 +1,160 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
+
+  <!--My CSS -->
+
+  <title>Seli Suhaeli Septian</title>
+</head>
+
+<body>
+  <!--navbar-->
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container">
+      <a class="navbar-brand" href="#">SeliSeptian</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav ml-auto">
+          <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link" href="#profile">Profile</a>
+          <a class="nav-item nav-link" href="#tugas">Kuliah</a>
+          <a class="nav-item nav-link" href="#tugas">Praktikum</a>
+          <a class="nav-item nav-link" href="#tugas">TuBes</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <!--akhir navbar-->
+
+  <!--jumbotron-->
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4">Welcome to My Website</h1>
+    </div>
+  </div>
+  <!--akhir jumbotron-->
+
+  <!--Container -->
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-10 info-panel">
+        <div class="row">
+          <div class="col-lg">
+            <img src="assets/img/1.png" alt="1" class="float-left">
+            <h4>Kuliah</h4>
+          </div>
+          <div class="col-lg">
+            <img src="assets/img/2.png" alt="2" class="float-left">
+            <h4>Praktikum</h4>
+          </div>
+          <div class="col-lg">
+            <img src="assets/img/3.jpg" alt="3" class="float-left">
+            <h4>Tugas Besar</h4>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+    <!--Profile-->
+    <section id="profile">
+      <h3>Profile</h3>
+      <div class="row">
+        <div class="col-4">
+          <img src="assets/img/seli.jpg" alt="seli" class="img-fluid">
+        </div>
+        <div class="col-8">
+          <h2>Hai!! Saya Seli Suhaeli Septian</h2>
+          <p>Lahir di Karawang, 25 September 2001. Saya mahasiswi dari Universitas Pasundan Fakultas Teknik Prodi Informatika </p>
+        </div>
+      </div>
+    </section>
+    <!--Akhir Profile-->
+
+    <!-- daftar tugas -->
+    <section id="tugas">
+      <h3>Tugas</h3>
+      <div class="row">
+        <div class="col">
+          <div class="card" style="width: 18rem; background-color:aqua;">
+            <div class="card-header" style="text-align: center;">Kuliah
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item"><a href="kuliah/pertemuan9">pertemuan 9</a></li>
+              <li class="list-group-item"><a href="kuliah/pertemuan10">pertemuan 10</a></li>
+              <li class="list-group-item"><a href="kuliah/pertemuan11">pertemuan 11</a></li>
+              <li class="list-group-item"><a href="kuliah/pertemuan12">pertemuan 12</a></li>
+              <li class="list-group-item"><a href="kuliah/pertemuan13">pertemuan 13</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col">
+          <img src="assets/img/1.png" alt="1" class="img-fluid">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <img src="assets/img/2.png" alt="2" class="img-fluid">
+        </div>
+        <div class="col">
+          <div class="card" style="width: 18rem; background-color:aqua;">
+            <div class="card-header" style="text-align: center;">Praktikum
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item"><a href="praktikum/P1_SENIN13_193040015">P1_SENIN13_193040015</a></li>
+              <li class="list-group-item"><a href="praktikum/P2_SENIN13_193040015">P2_SENIN13_193040015</a></li>
+              <li class="list-group-item"><a href="praktikum/P3_SENIN13_193040015">P3_SENIN13_193040015</a></li>
+              <li class="list-group-item"><a href="praktikum/P4_SENIN13_193040015">P4_SENIN13_193040015</a></li>
+              <li class="list-group-item"><a href="praktikum/P5_SENIN13_193040015">P5_SENIN13_193040015</a></li>
+              <li class="list-group-item"><a href="praktikum/P6_SENIN13_193040015">P6_SENIN13_193040015</a></li>
+              <li class="list-group-item"><a href="praktikum/P7_SENIN13_193040015">P7_SENIN13_193040015</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <div class="card" style="width: 18rem; background-color:aqua;">
+            <div class="card-header" style="text-align: center;">Tugas Besar</div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item"><a href="tubes/index.php">Tugas Besar</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col">
+          <img src="assets/img/3.jpg" alt="1" class="img-fluid">
+        </div>
+      </div>
+    </section>
+    <!-- akhir daftar tugas -->
+  </div>
+
+  <!--Akhir Container -->
+
+  <!-- footer -->
+  <footer>
+    <p style="text-align: center; background-color:black; color:beige;">&copy; 2020 SeliSeptian </p>
+  </footer>
+  <!-- akhir footer -->
+
+
+
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+
+</html>
